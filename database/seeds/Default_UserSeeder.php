7ee128904f1ba824152876feaed16a4c31fc0b29
aungkokhant <aungkokhantace@gmail.com>
2018-05-03 15:21:54 +0630

    modify user name for default users

--- database/seeds/Default_UserSeeder.php
+++ database/seeds/Default_UserSeeder.php
@@ -22,9 +22,9 @@ class Default_UserSeeder extends Seeder
       ['id'=>1, 'user_name'=>'administrator_ab','display_name'=>'Administrator', 'password' =>'$2y$10$aFsFMkqNMP5kx0EyCs1wuOGu/je0OvLAhKXezURMLeYg/V1IztxwG', 'email' =>'administrator_ab@gmail.com','role_id' =>'1','staff_id'=>'9999','address'=>'Building 5, Room 10, MICT Park, Hlaing Township, Yangon, Myanmar.','description'=>'This is super admin first login role'],
       //pwd for user_id = 3 => 1@#@auderbox
 
-      ['id'=>2, 'user_name'=>'admin_aceplus','display_name'=>'Administrator', 'password' =>'$2y$10$NLS2i1NpEUuQgoLgYk0QSOxsKxk6u1PFdeYJkpCraq2rS6polwYI6', 'email' =>'waiyanaung@aceplussolutions.com','role_id' =>'2','staff_id'=>'0001','address'=>'Building 5, Room 10, MICT Park, Hlaing Township, Yangon, Myanmar','description'=>'This is super admin role'],
+      ['id'=>2, 'user_name'=>'admin_aceplus','display_name'=>'Super Admin', 'password' =>'$2y$10$NLS2i1NpEUuQgoLgYk0QSOxsKxk6u1PFdeYJkpCraq2rS6polwYI6', 'email' =>'waiyanaung@aceplussolutions.com','role_id' =>'2','staff_id'=>'0001','address'=>'Building 5, Room 10, MICT Park, Hlaing Township, Yangon, Myanmar','description'=>'This is super admin role'],
 
-      ['id'=>3, 'user_name'=>'admin_auderbox','display_name'=>'Starfish Administrator', 'password' =>'$2y$10$y57lFUd93SHOmCWJ8lZYROtnWQMxK90uos31q17Lhgfmc4fzgaDO6', 'email' =>'admin_auderbox@gmail.com','role_id' =>'3','staff_id'=>'0002','address'=>'','description'=>'This is admin role'],
+      ['id'=>3, 'user_name'=>'admin_auderbox','display_name'=>'Auderbox Admin', 'password' =>'$2y$10$y57lFUd93SHOmCWJ8lZYROtnWQMxK90uos31q17Lhgfmc4fzgaDO6', 'email' =>'admin_auderbox@gmail.com','role_id' =>'3','staff_id'=>'0002','address'=>'','description'=>'This is admin role'],
       //pwd for user_id = 2 => 123@auderbox
 
     );
