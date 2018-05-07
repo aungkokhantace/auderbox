b879a82791b96774a4d8df4e9684d196afe75def
aungkokhant <aungkokhantace@gmail.com>
2018-05-07 17:25:55 +0630

    handle user does not exist case

--- app/Api/RetailerProfile/RetailerProfileApiRepository.php
+++ app/Api/RetailerProfile/RetailerProfileApiRepository.php
@@ -21,11 +21,18 @@ class RetailerProfileApiRepository implements RetailerProfileApiRepositoryInterf
 
       try {
         $user = User::find($id);
+        if(isset($user) && count($user)>0){
+          $returnedObj['aceplusStatusCode']     = ReturnMessage::OK;
+          $returnedObj['aceplusStatusMessage']  = "Request is successful!";
+          $returnedObj['userObj']               = $user;
+          return $returnedObj;
+        }
+        else{
+          //if user does not exist
+          $returnedObj['aceplusStatusMessage']  = "User does not exist!";
+          return $returnedObj;
+        }
 
-        $returnedObj['aceplusStatusCode']     = ReturnMessage::OK;
-        $returnedObj['aceplusStatusMessage']  = "Request is successful!";
-        $returnedObj['userObj']               = $user;
-        return $returnedObj;
       }
       catch(\Exception $e){
           $returnedObj['aceplusStatusMessage'] = $e->getMessage();
