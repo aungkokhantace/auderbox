4ebd50458a0275f371df76e9b0a68b60598b1667
aungkokhant <aungkokhantace@gmail.com>
2018-05-18 17:58:16 +0630

    add delivery date calculation api

---
+++ app/Backend/BrandOwnerDeliveryBlackoutDay/BrandOwnerDeliveryBlackoutDay.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Author: Aung Ko Khant
+ * Date: 2018-05-17
+ * Time: 03:45 PM
+ */
+
+namespace App\Backend\BrandOwnerDeliveryBlackoutDay;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BrandOwnerDeliveryBlackoutDay extends Model
+{
+    protected $table = 'brand_owner_delivery_blackout_day';
+
+    protected $casts = [
+        'id' => 'integer',
+        'brand_owner_id' => 'integer',
+        'name' => 'string',
+        'date' => 'string',
+        'type' => 'integer',
+        'remark' => 'string',
+        'status' => 'integer',
+        'created_by' => 'integer',
+        'updated_by' => 'integer',
+        'deleted_by' => 'integer',
+        'created_at' => 'timestamp',
+        'updated_at' => 'timestamp',
+        'deleted_at' => 'timestamp'
+   ];
+
+    protected $fillable = [
+        'id',
+        'brand_owner_id',
+        'name',
+        'date',
+        'type',
+        'remark',
+        'status',
+        'created_by','updated_by','deleted_by','created_at','updated_at','deleted_at'
+    ];
+
+
+}
